162225f9b4376b3e6c751e56cf187216fa73f3cd
Ksenia Zemskova <kv.zemskova@gmail.com>
2022-08-21 00:58:09 +0300

    Добавлена конструкция if __name__ == __main__ в файл main.py

--- sqlite_to_postgres/main.py
+++ sqlite_to_postgres/main.py
@@ -2,15 +2,15 @@ import sqlite_connector
 import pg_connector
 
 
-film_works = sqlite_connector.read_film_work()
-people = sqlite_connector.read_person()
-genres = sqlite_connector.read_genre()
-genre_film_work = sqlite_connector.read_genre_film_work()
-person_film_work = sqlite_connector.read_person_film_work()
+if __name__ == '__main__':
+    film_works = sqlite_connector.read_film_work()
+    people = sqlite_connector.read_person()
+    genres = sqlite_connector.read_genre()
+    genre_film_work = sqlite_connector.read_genre_film_work()
+    person_film_work = sqlite_connector.read_person_film_work()
 
-
-pg_connector.write_person(people)
-pg_connector.write_film_work(film_works)
-pg_connector.write_genre(genres)
-pg_connector.write_genre_film_work(genre_film_work)
-pg_connector.write_person_film_work(person_film_work)
+    pg_connector.write_person(people)
+    pg_connector.write_film_work(film_works)
+    pg_connector.write_genre(genres)
+    pg_connector.write_genre_film_work(genre_film_work)
+    pg_connector.write_person_film_work(person_film_work)
